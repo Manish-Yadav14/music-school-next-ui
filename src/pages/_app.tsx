ccc322a08b9fec8a8fc35071c27de039e8739b19
Manish Yadav <manishyadav14127@gmail.com>
2024-12-31 13:55:23 +0530

    project added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Navbar from "@/components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return(
+    <>
+      <div className="relative w-full flex items-center justify-center ">
+        <Navbar />
+      </div>
+      <Component {...pageProps} />
+    </>
+  )
 }
